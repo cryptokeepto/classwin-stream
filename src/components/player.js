6a425c05d7b260e321f361a88eb5d5ed0ecea1e9
cryptokeepto <cryptokeepto@gmail.com>
2019-07-20 14:45:26 +0700

    add button live

--- src/components/player.js
+++ src/components/player.js
@@ -11,6 +11,21 @@ const PlayerInner = styled.div`
 
 `
 
+const VideoTitle = styled.h2`
+  font-size: 22px;
+  line-height: 25px;
+`
+
+const VideoLiveButton = styled.span`
+  display: inline;
+  padding 7px;
+  color: white;
+  margin-right: 10px;
+  background-color: red;
+  line-height: 25px;
+  font-size: 14px;
+`
+
 class Player extends Component {
   
   componentDidMount() {
@@ -41,6 +56,10 @@ class Player extends Component {
       <PlayerWrapper>
         <PlayerInner>
           <video style={style} ref={(player) => this.player = player} controls poster="https://linkassistive.com/wp-content/uploads/2012/12/big-buck-bunny-poster.png" />
+          <VideoTitle>
+            <VideoLiveButton>Live</VideoLiveButton>
+            Big Buck Bunny
+          </VideoTitle>
         </PlayerInner>
       </PlayerWrapper>
     )
